235ba8dc35afc931adbe80d3f33907b23dff8654
Janisa Sri S <jani2995@global.chainsys.com>
2022-03-08 11:21:56 +0530

    ping commit

---
+++ src/ping/ping-coming-soon-page-master/ping.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+'use strict';
+
+const inputEmail=document.getElementById("input-email");
+const submitBtn=document.getElementById("submit-btn");
+const form=document.getElementById("form");
+
+submitBtn.addEventListener('click',(e) =>{
+    e.preventDefault();
+    const regEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if(inputEmail.value.match(regEx)){
+        console.log("valid");
+        form.classList.remove("error");
+    }else {
+        console.log("error");
+        form.classList.add("error");
+    }
+});
