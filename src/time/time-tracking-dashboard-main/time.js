2513a61a7b22791c9829aeb278b5c1230d948f89
Janisa Sri S <jani2995@global.chainsys.com>
2022-03-09 01:09:50 +0530

    new commit

---
+++ src/time/time-tracking-dashboard-main/time.js
@@ -0,0 +1,6 @@
+/*eslint-disable*/
+const daily=document.getElementById('daily');
+const weekly=document.getElementById('weekly');
+const monthly=document.getElementById('monthly');
+let boxContent;
+daily.addEventListener('click',getDailyReport);
